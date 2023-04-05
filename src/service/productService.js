4844eb3f0393c843197550ff317d0fbfbd74c7d0
nguyennhattan128 <nguyennhattanjp@gmail.com>
2023-04-05 14:09:58 +0700

    ket noi du lieu thanh cong

---
+++ src/service/productService.js
@@ -0,0 +1,24 @@
+const connection = require('../entity/connection');
+
+class ProductService{
+    connect;
+    constructor() {
+        connection.connectToMySQL();
+        this.connect = connection.getConnection()
+    }
+    findAll = () => {
+      return new Promise((resolve,reject) => {
+          this.connect.query(`select *, category.nameCategory from product join category
+    on product.idCategory = category.id`,(err,products) => {
+              if(err){
+                  reject(err)
+              }else {
+                  resolve(products)
+                  console.log('thanh cong')
+              }
+          })
+      })
+}
+}
+let product = new ProductService();
+product.findAll();
