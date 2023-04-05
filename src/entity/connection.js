b51d3b4f52d63ab97a900cfcdca62f855609d18d
Ducanh1994 <121839766+Ducanh1994@users.noreply.github.com>
2023-04-05 10:00:17 +0700

    dev tao connection-10:00-4/5/23

---
+++ src/entity/connection.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+class Connection {
+    configToMySQL = {
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'stores',
+        charset: 'utf8_general_ci'
+    }
+    getconnection = () => {
+        return mysql.createConnection(this.configToMySQL);
+    }
+    connectToMySQL = () => {
+        this.getconnection().connect((err) => {
+            if (err) {
+                console.log(err);
+            }
+            else  {
+                console.log('Connect database success')
+            }
+        })
+    }
+}
+
+module.exports = new Connection();
